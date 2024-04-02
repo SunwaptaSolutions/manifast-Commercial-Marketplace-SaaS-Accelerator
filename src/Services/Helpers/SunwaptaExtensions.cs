e2eb9b8e9c6bdfb2b71706e95a4fe72f8ad844a0
Glen Little <glen.little@gmail.com>
2024-04-02 10:52:34 -0600

    add direct to SendGrid emails

---
+++ src/Services/Helpers/SunwaptaExtensions.cs
@@ -0,0 +1,94 @@
+﻿using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.SaaS.Accelerator.Services.Helpers;
+public static class SunwaptaExtensions
+{
+  // return the innermost exception's message
+  public static string GetBaseMessage(this Exception ex)
+  {
+    while (ex.InnerException != null)
+    {
+      ex = ex.InnerException;
+    }
+    return ex.Message;
+  }
+
+  // convert .NET MailAddress to SendGrid EmailAddress
+  public static EmailAddress AsSendGridEmailAddress(this MailAddress input)
+  {
+    return new EmailAddress(input.Address, input.DisplayName);
+  }
+
+  // convert a string to SendGrid EmailAddress
+  public static EmailAddress AsSendGridEmailAddress(this string input)
+  {
+    return new EmailAddress(input);
+  }
+
+
+  /// <summary>
+  ///   For an array of strings, join them.
+  /// </summary>
+  public static string JoinedAsString(this IEnumerable<string> array, string separator, Func<string, string> converter)
+  {
+    return array.AsEnumerable().Select(converter).JoinedAsString(separator, string.Empty, string.Empty);
+  }
+
+
+  /// <summary>
+  ///   Not IsNullOrEmpty
+  /// </summary>
+  [DebuggerStepThrough]
+  public static bool HasContent(this string input)
+  {
+    return !string.IsNullOrEmpty(input);
+  }
+
+
+  public static string JoinedAsString(this IEnumerable<string> array, string separator, bool skipBlanks)
+  {
+    return array.AsEnumerable()
+      .Where(s => !skipBlanks || s.HasContent())
+      .JoinedAsString(separator, string.Empty, string.Empty);
+  }
+
+  /// <summary>
+  ///   For an enumeration of strings, join them.
+  /// </summary>
+  public static string JoinedAsString(this IEnumerable<string> list)
+  {
+    return JoinedAsString(list, string.Empty);
+  }
+
+  /// <summary>
+  ///   For an enumeration of strings, join them.
+  /// </summary>
+  public static string JoinedAsString(this IEnumerable<string> list, string separator)
+  {
+    return list.JoinedAsString(separator, string.Empty, string.Empty);
+  }
+
+  /// <summary>
+  ///   For an enumeration of strings, join them. Each item has itemLeft and itemRight added.
+  /// </summary>
+  public static string JoinedAsString(this IEnumerable<string> list, string separator, string itemLeft,
+    string itemRight, bool skipBlanks = false)
+  {
+    if (list == null)
+    {
+      return string.Empty;
+    }
+    var list2 = list.ToList();
+    return list2.Any()
+      ? string.Join(separator, list2.Where(s => !skipBlanks || s.HasContent()).Select(s => itemLeft + s + itemRight).ToArray())
+      : "";
+  }
+
+}
--- src/Services/Services/SMTPEmailService.cs
+++ src/Services/Services/SMTPEmailService.cs
@@ -1,8 +1,17 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using Marketplace.SaaS.Accelerator.DataAccess.Contracts;
 using Marketplace.SaaS.Accelerator.Services.Contracts;
+using Marketplace.SaaS.Accelerator.Services.Helpers;
 using Marketplace.SaaS.Accelerator.Services.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using ILogger = Marketplace.SaaS.Accelerator.Services.Contracts.ILogger;
 
 namespace Marketplace.SaaS.Accelerator.Services.Services;
 
@@ -12,56 +21,125 @@ namespace Marketplace.SaaS.Accelerator.Services.Services;
 /// <seealso cref="IEmailService" />
 public class SMTPEmailService : IEmailService
 {
-    /// <summary>
-    /// The application configuration repository.
-    /// </summary>
-    private readonly IApplicationConfigRepository applicationConfigRepository;
+  /// <summary>
+  /// The application configuration repository.
+  /// </summary>
+  // private readonly IApplicationConfigRepository applicationConfigRepository;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="SMTPEmailService"/> class.
-    /// </summary>
-    /// <param name="applicationConfigRepository">The application configuration repository.</param>
-    public SMTPEmailService(IApplicationConfigRepository applicationConfigRepository)
+  private readonly ILogger<SMTPEmailService> Logger;
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="SMTPEmailService"/> class.
+  /// </summary>
+  /// <param name="logger"></param>
+  public SMTPEmailService()
+  {
+    var loggerFactory = LoggerFactory.Create(builder =>
     {
-        this.applicationConfigRepository = applicationConfigRepository;
+      builder
+        .AddConsole();
+    });
+
+
+    // this.applicationConfigRepository = applicationConfigRepository;
+    Logger = loggerFactory.CreateLogger<SMTPEmailService>();
+  }
+
+  /// <summary>
+  /// Sends the email.
+  /// </summary>
+  /// <param name="emailContent">Content of the email.</param>
+  public void SendEmail(EmailContentModel emailContent)
+  {
+    SendViaSendGrid(emailContent);
+
+    // MailMessage mail = new MailMessage();
+    // if (!string.IsNullOrEmpty(emailContent.ToEmails) || !string.IsNullOrEmpty(emailContent.BCCEmails))
+    // {
+    //   mail.From = new MailAddress(emailContent.FromEmail);
+    //   mail.IsBodyHtml = true;
+    //   mail.Subject = emailContent.Subject;
+    //   mail.Body = emailContent.Body;
+    //
+    //   string[] toEmails = emailContent.ToEmails.Split(';');
+    //   foreach (string multimailid in toEmails)
+    //   {
+    //     mail.To.Add(new MailAddress(multimailid));
+    //   }
+    //
+    //   if (!string.IsNullOrEmpty(emailContent.BCCEmails))
+    //   {
+    //     foreach (string multimailid1 in toEmails)
+    //     {
+    //       mail.Bcc.Add(new MailAddress(multimailid1));
+    //     }
+    //   }
+    //
+    //   SmtpClient smtp = new SmtpClient();
+    //   smtp.Host = emailContent.SMTPHost;
+    //   smtp.Port = emailContent.Port;
+    //   smtp.UseDefaultCredentials = true;
+    //   smtp.Timeout = 10000; // 10 seconds
+    //   smtp.Credentials = new NetworkCredential(
+    //       emailContent.UserName, emailContent.Password);
+    //   smtp.EnableSsl = emailContent.SSL;
+    //   try
+    //   {
+    //     smtp.Send(mail);
+    //   }
+    //   catch (Exception ex)
+    //   {
+    //     throw new Exception($"Email failed: {ex.Message}");
+    //   }
+    //   finally
+    //   {
+    //     mail.Dispose();
+    //     smtp.Dispose();
+    //   }
+    // }
+  }
+
+  private bool SendViaSendGrid(EmailContentModel emailContent) // string sendGridApiKey, MailMessage message, out string errorMessage)
+  {
+    var sendGridApiKey = emailContent.Password;
+
+    var msg = new SendGridMessage
+    {
+      From = emailContent.FromEmail.AsSendGridEmailAddress(),
+      Subject = emailContent.Subject,
+      HtmlContent = emailContent.Body  //htmlBody
+    };
+    msg.AddTos(emailContent.ToEmails.Split(new []{',', ';' }, StringSplitOptions.RemoveEmptyEntries).Select(a => a.AsSendGridEmailAddress()).ToList());
+
+    var ccList = emailContent.CCEmails.Split(new[] {',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+    if (ccList.Length > 0)
+    {
+      msg.AddCcs(ccList.Select(a => a.AsSendGridEmailAddress()).ToList());
     }
 
-    /// <summary>
-    /// Sends the email.
-    /// </summary>
-    /// <param name="emailContent">Content of the email.</param>
-    public void SendEmail(EmailContentModel emailContent)
+    var bccList = emailContent.BCCEmails.Split(new[] {',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+    if (bccList.Length > 0)
     {
-        MailMessage mail = new MailMessage();
-        if (!string.IsNullOrEmpty(emailContent.ToEmails) || !string.IsNullOrEmpty(emailContent.BCCEmails))
-        {
-            mail.From = new MailAddress(emailContent.FromEmail);
-            mail.IsBodyHtml = true;
-            mail.Subject = emailContent.Subject;
-            mail.Body = emailContent.Body;
-
-            string[] toEmails = emailContent.ToEmails.Split(';');
-            foreach (string multimailid in toEmails)
-            {
-                mail.To.Add(new MailAddress(multimailid));
-            }
-
-            if (!string.IsNullOrEmpty(emailContent.BCCEmails))
-            {
-                foreach (string multimailid1 in toEmails)
-                {
-                    mail.Bcc.Add(new MailAddress(multimailid1));
-                }
-            }
-
-            SmtpClient smtp = new SmtpClient();
-            smtp.Host = emailContent.SMTPHost;
-            smtp.Port = emailContent.Port;
-            smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new NetworkCredential(
-                emailContent.UserName, emailContent.Password);
-            smtp.EnableSsl = emailContent.SSL;
-            smtp.Send(mail);
-        }
+      msg.AddBccs(bccList.Select(a => a.AsSendGridEmailAddress()).ToList());
     }
+
+    var sendGridClient = new SendGridClient(sendGridApiKey);
+    var response = sendGridClient.SendEmailAsync(msg).Result;
+
+    if (response.IsSuccessStatusCode)
+    {
+      return true;
+    }
+
+    // failed
+    var details = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(response.Body.ReadAsStringAsync().Result);
+    var errorList = details?.Select(x => $"{x.Key} = {x.Value}").JoinedAsString("\r\n");
+
+    var fullSubject = $"EmailApi Failed ({Environment.MachineName})";
+    var errorMessage = $"{fullSubject}\r\n{response.StatusCode}\r\n\r\n{errorList}";
+
+    Logger.LogError(errorMessage);
+    return false;
+
+  }
 }
